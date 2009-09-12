fb53886b45843ce9fa175b8f323f8976f46fdd29
Leslie P. Polzer <polzer@gnu.org>
2009-09-12 12:34:54 +0200

    Amended dev-install.sh.

--- dev-install.sh
+++ dev-install.sh
@@ -3,7 +3,11 @@ set -e
 
 ./build-tarball.sh
 makepkg -g >> PKGBUILD
+mkdir -p pkg
 cd pkg
+ln -sf ../PKGBUILD
+ln -sf ../paktahn.install
+ln -sf ../paktahn-0.8.tar.bz2
 makepkg -f
 sudo pacman -U paktahn*.pkg.tar.gz
 cd ..
