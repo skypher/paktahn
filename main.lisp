994fd21cb6652ec285cb3bdf2d5d90440a02fab2
Brit Butler <redline6561@gmail.com>
2009-11-21 15:44:25 -0500

    Adjusted info argument to print directory correctly in -G clause of main.

--- main.lisp
+++ main.lisp
@@ -324,7 +324,7 @@ Usage:
      (mapcar #'get-pkgbuild (cdr argv))
      (mapcar #'(lambda (pkg-name)
 		 (info "The ~a pkgbuild is in ~a.~%" pkg-name
-		       (concatenate 'string (current-directory) "/" pkg-name "/")))
+		       (concatenate 'string (namestring (current-directory)) pkg-name "/")))
 	     (cdr argv)))
     (t
      (display-help))))
