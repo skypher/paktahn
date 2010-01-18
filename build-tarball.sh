c497af3fcb9315cfa59dc6810ae29187f3991b65
Leslie P. Polzer <polzer@gnu.org>
2010-01-18 19:09:22 +0100

    Sync build, fix style warning.

--- build-tarball.sh
+++ build-tarball.sh
@@ -1,5 +1,5 @@
 source ./PKGBUILD
 tar cvfj paktahn-$pkgver.tar.bz2 \
-  {alpm,aur,main,pkgbuild,util,readline,term,pyconfig-fix,cache,checksums,packages,customizepkg}.lisp \
+  {alpm,aur,main,pkgbuild,util,readline,term,pyconfig-fix,cache,checksums,packages,customizepkg,safe-foreign-string}.lisp \
   paktahn.asd \
   pkgbuild-helper.sh makepkg-helper.sh
--- packages.lisp
+++ packages.lisp
@@ -9,4 +9,5 @@
       (:use :cl :cffi)
       (:import-from :alexandria :compose :curry :rcurry :ensure-list)
       (:import-from :metatilities :push-end :aif :it)
-      (:import-from :usocket :split-sequence)))
+      (:import-from :split-sequence :split-sequence)))
+
