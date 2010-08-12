e1de69fc60e6783001c6066e3e0c3a29e022e659
Brit Butler <redline6561@gmail.com>
2010-08-12 17:51:37 -0400

    Cleanup: Improved checksumming UI.

--- checksums.lisp
+++ checksums.lisp
@@ -1,11 +1,9 @@
 (in-package :pak)
 
-;; Right now, this data structure choice seems good but the choice of pkg-name for a key
-;; could make extending the checksumming to files other than PKGBUILDs messy later.
 (defvar *checksums* (make-hash-table :test #'equal)
   "Hash table containing checksums of PKGBUILDs.
 The package names are the keys. The checksum byte arrays
-are the values. Support will be added for non-PKGBUILD files later.")
+are the values.")
 
 (defun load-checksums ()
   (let ((checksum-file (config-file "checksums")))
@@ -19,16 +17,18 @@ are the values. Support will be added for non-PKGBUILD files later.")
 
 (defun compare-checksums (pkg-name)
   (let ((pkgbuild-md5 (md5sum-file "PKGBUILD"))
-	(old-md5s (lookup-checksum pkg-name)))
-    (cond ((not old-md5s)  ; if new PKGBUILD, ask the user to review it and add it to the checksum-db
-	   (prompt-user-review "PKGBUILD")
-	   (add-checksum pkg-name pkgbuild-md5))
-	  ((and (new-checksum-p pkgbuild-md5 old-md5s) ; otherwise, compare its md5sum to that on record and prompt the user if necessary
-		(ask-y/n "The PKGBUILD checksum doesn't match our records. Review the PKGBUILD?" t))
-	   (launch-editor "PKGBUILD")
-	   (add-checksum pkg-name pkgbuild-md5))
+	(old-md5s (lookup-checksum pkg-name))
+        (review-p t))
+    (cond ((not old-md5s)
+           (info "This is a new PKGBUILD."))
+	  ((new-checksum-p pkgbuild-md5 old-md5s)
+           (info "The PKGBUILD has changed since you saw it last."))
           (t
-           (info "You have already reviewed this PKGBUILD file")))))
+           (info "You have already reviewed this PKGBUILD")
+           (setf review-p nil)))
+    (when (and review-p (ask-y/n "Review it" t))
+      (launch-editor "PKGBUILD"))
+    (add-checksum pkg-name pkgbuild-md5)))
 
 (defun add-checksum (pkg-name checksum)
   (pushnew checksum (gethash pkg-name *checksums*) :test #'equalp))
@@ -38,4 +38,3 @@ are the values. Support will be added for non-PKGBUILD files later.")
 
 (defun new-checksum-p (new old)
   (not (member new old :test #'equalp)))
-
